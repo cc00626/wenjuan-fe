7fb4677ddbe7d1605583ea67afbf2114dfe40f1e
cc <2684923343@qq.com>
2025-05-25 19:12:56 +0800

    chore: 点击组件添加到画布总

--- src/components/ComponentLib/index.tsx
+++ src/components/ComponentLib/index.tsx
@@ -2,23 +2,41 @@ import React, {FC} from 'react'
 import {ComponentConfigType, componentGroup, ComponentGroupType} from '../QuestionComponents'
 import {Typography} from 'antd'
 import style from './index.module.scss'
+import {useDispatch} from 'react-redux'
+import {addComponent} from '../../store/component'
+import {nanoid} from 'nanoid'
 const {Title} = Typography
 
-//根据类型选择组件
-const GetComponent = (c: ComponentGroupType) => {
-  const {componentConfig} = c
-  return (
-    <div className={style.component}>
-      <div className={style.stopclick}>
-        {componentConfig.map((item: ComponentConfigType) => {
-          const {Component} = item
-          return <Component key={item.type} />
-        })}
-      </div>
-    </div>
-  )
-}
 const ComponentLib: FC = () => {
+  const dispatch = useDispatch()
+  const addToCanvas = (item: ComponentGroupType) => {
+    const {componentConfig} = item
+    //找到组件的信息
+    componentConfig.map((item: ComponentConfigType) => {
+      const {title, type, defaultProps} = item
+      dispatch(addComponent({fe_id: nanoid(), title, type, props: defaultProps}))
+    })
+  }
+
+  //根据类型选择组件
+  const GetComponent = (c: ComponentGroupType) => {
+    const {componentConfig} = c
+    return (
+      <div
+        className={style.component}
+        onClick={() => {
+          addToCanvas(c)
+        }}
+      >
+        <div className={style.stopclick}>
+          {componentConfig.map((item: ComponentConfigType) => {
+            const {Component} = item
+            return <Component key={item.type} />
+          })}
+        </div>
+      </div>
+    )
+  }
   //添加静态组件
   return (
     <div>
@@ -28,6 +46,7 @@ const ComponentLib: FC = () => {
             <Title level={3} className={style.title} style={index === 0 ? {marginTop: '0'} : {}}>
               {item.groupName}
             </Title>
+
             {GetComponent(item)}
           </div>
         )
--- src/store/component/index.ts
+++ src/store/component/index.ts
@@ -24,12 +24,26 @@ const createReducer = createSlice({
     resetComponent: (state: ComponentStateType, action: PayloadAction<ComponentStateType>) => {
       return action.payload
     },
+    //点击选中组件
     changeSelectId: (state: ComponentStateType, action: PayloadAction<string>) => {
       state.selectId = action.payload
+      return state
+    },
+    //从组件库中添加组件到画布中
+    addComponent: (state: ComponentStateType, action: PayloadAction<ComponentInfoType>) => {
+      if (state.selectId === '') {
+        //添加到最后一位
+        state.componentList.push(action.payload)
+      } else {
+        //添加到选中组件的后面
+        const index = state.componentList.findIndex(item => item.fe_id === state.selectId)
+        state.componentList.splice(index + 1, 0, action.payload)
+      }
+      state.selectId = action.payload.fe_id
       return state
     },
   },
 })
 
-export const {resetComponent, changeSelectId} = createReducer.actions
+export const {resetComponent, changeSelectId, addComponent} = createReducer.actions
 export default createReducer.reducer
